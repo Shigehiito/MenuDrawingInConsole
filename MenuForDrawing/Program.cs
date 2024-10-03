7c904343cc9dc132c793dc0855e2900cb38c88da
Gergő Zsombor Csányi <csanyi.gergo.zsombor@blathy.info>
2024-10-03 12:29:53 +0200

    Add project files.

---
+++ MenuForDrawing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace MenuForDrawing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int caseSwitch = 1;
+            Console.SetCursorPosition(0, 0);
+            do
+            {
+                switch (caseSwitch)
+                {
+                    case ConsoleKey.Escape:
+                        break;
+                }
+
+                Console.Write('╔');
+                for (int i = 1; i <= Console.WindowWidth - 2; i++)
+                {
+                    Console.Write('═');
+                }
+                Console.Write('╗');
+                for (int i = 1; i <= Console.WindowHeight - 2; i++)
+                {
+                    Console.WriteLine('║');
+                }
+                Console.Write('╚');
+                for (int i = 1; i <= Console.WindowWidth - 2; i++)
+                {
+                    Console.Write('═');
+                }
+                Console.Write('╝');
+                for (int i = 1; i <= Console.WindowHeight - 2; i++)
+                {
+                    Console.SetCursorPosition(Console.WindowWidth - 1, i);
+                    Console.Write('║');
+                }
+
+
+            }
+            while (input.Key != ConsoleKey.Escape);
+
+
+
+        }
+    }
+}
