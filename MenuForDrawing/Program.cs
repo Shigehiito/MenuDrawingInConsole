4b98a63c583347cf34b2ce57bdbcce185baf4e67
Shigehiito <csanyi.gergo05@gmail.com>
2024-10-13 18:33:12 +0200

    Finalised the Buttons, and it's working.

--- MenuForDrawing/Program.cs
+++ MenuForDrawing/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace MenuForDrawing
 {
     internal class Program
     {
+        static bool hasPreviousDrawing = false;
+        static List<(int x, int y, char character, ConsoleColor color)> trail = new List<(int, int, char, ConsoleColor)>();
+
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -26,21 +29,52 @@ namespace MenuForDrawing
                         break;
 
                     case ConsoleKey.DownArrow:
-                        if (selectedButton < 4)
+                        if (selectedButton < 5)
                             selectedButton++;
                         break;
 
                     case ConsoleKey.Enter:
-                        Console.SetCursorPosition(0, Console.WindowHeight - 2);
+                        Console.SetCursorPosition(2, Console.WindowHeight - 1);
                         Console.WriteLine($"Gomb {selectedButton} kiválasztva!");
 
                         switch (selectedButton)
                         {
-                            case 1: 
+                            case 1:
                                 Console.Clear();
-                                DrawInConsole();
+                                DrawBorder();
+                                DrawInConsole(false);
                                 break;
-                            case 4: 
+                            case 2:
+                                Console.Clear();
+                                DrawBorder();
+                                openFolder();
+                                break;
+                            case 3:
+                                if (hasPreviousDrawing)
+                                {
+
+                                    Console.Clear();
+                                    DrawBorder();
+                                    DrawInConsole(true);
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    DrawBorder();
+                                    openFolder();
+                                }
+                                break;
+                            case 4:
+                                Console.Clear();
+                                DrawBorder();
+                                Console.SetCursorPosition(0, Console.WindowHeight - 2);
+                                Console.WriteLine("Add meg a fájl nevét, amiként menteni szeretnéd a rajzot: ");
+                                string savePath = Console.ReadLine();
+                                SaveDrawingToFile(savePath);
+                                Console.Clear();
+                                DrawBorder();
+                                break;
+                            case 5:
                                 Environment.Exit(0);
                                 break;
                         }
@@ -56,6 +90,107 @@ namespace MenuForDrawing
             } while (input.Key != ConsoleKey.Escape);
         }
 
+        static void openFolder()
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            string folderPath = "C:\\Users\\csany\\source\\repos\\MenuDrawingIntoConsole\\MenuForDrawing\\bin\\Debug\\net8.0\\Drawings";
+
+            try
+            {
+                if (Directory.Exists(folderPath))
+                {
+                    string[] files = Directory.GetFiles(folderPath);
+                    Console.Clear();
+                    Console.WriteLine("Elérhető fájlok:");
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {Path.GetFileName(files[i])}");
+                    }
+
+                    Console.WriteLine("Válaszd ki a fájlt a szám megadásával (vagy nyomd meg az Esc billentyűt a visszatéréshez): ");
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        Console.Clear();
+                        return; 
+                    }
+
+                    if (int.TryParse(keyInfo.KeyChar.ToString(), out int fileIndex) && fileIndex > 0 && fileIndex <= files.Length)
+                    {
+                        string selectedFile = files[fileIndex - 1];
+                        LoadDrawingFromFile(selectedFile);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Érvénytelen választás.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("A megadott mappa nem található.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a mappa megnyitása során:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void LoadDrawingFromFile(string folderPath)
+        {
+            try
+            {
+                string[] fileContent = File.ReadAllLines(folderPath);
+
+                Console.Clear();
+                trail.Clear();  
+
+                foreach (var line in fileContent)
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 4)
+                    {
+                        int x = int.Parse(parts[0]);
+                        int y = int.Parse(parts[1]);
+                        char character = char.Parse(parts[2]);
+                        ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), parts[3]);
+
+                        trail.Add((x, y, character, color));
+                        DrawCharacter(x, y, character, color);
+                    }
+                }
+
+                hasPreviousDrawing = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a fájl beolvasása során:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void SaveDrawingToFile(string folderPath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(folderPath))
+                {
+                    foreach (var item in trail)
+                    {
+                        writer.WriteLine($"{item.x},{item.y},{item.character},{item.color}");
+                    }
+                }
+                Console.WriteLine("A rajz sikeresen elmentve.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a mentés során:");
+                Console.WriteLine(ex.Message);
+            }
+        }
         static void DrawBorder()
         {
             Console.Write('╔');
@@ -83,11 +218,11 @@ namespace MenuForDrawing
 
         static void DrawButtons(int selectedButton)
         {
-            string[] buttons = { "Gomb 1 - Új rajzoló felület", "Gomb 2", "Gomb 3", "Gomb 4 - Kilépés" };
+            string[] buttons = { "Új Felület Nyitása", "Elözö Munka Betöltése", "Rajzolás Folytatása", "Fájl Mentése Másként", "Kilépés" };
 
             int buttonWidth = 30;
             int buttonHeight = 4;
-            int verticalSpacing = 2;
+            int verticalSpacing = 1;
 
             int startX = (Console.WindowWidth - buttonWidth) / 2;
             int startY = (Console.WindowHeight - (buttons.Length * (buttonHeight + verticalSpacing))) / 2;
@@ -136,34 +271,38 @@ namespace MenuForDrawing
         static int prevX = 10, prevY = 10;
         static ConsoleColor currentColor = ConsoleColor.White;
         static char currentChar = '█';
-        static char prevChar = ' ';
-
-        static List<(int x, int y, char character, ConsoleColor color)> trail = new List<(int, int, char, ConsoleColor)>();
-
 
         static Dictionary<ConsoleKey, char> charMapping = new Dictionary<ConsoleKey, char>
-    {
-        { ConsoleKey.F1, '█' },
-        { ConsoleKey.F2, '▓' },
-        { ConsoleKey.F3, '▒' },
-        { ConsoleKey.F4, '░' }
-    };
+        {
+            { ConsoleKey.F1, '█' },
+            { ConsoleKey.F2, '▓' },
+            { ConsoleKey.F3, '▒' },
+            { ConsoleKey.F4, '░' }
+        };
 
         static Dictionary<ConsoleKey, ConsoleColor> colorMapping = new Dictionary<ConsoleKey, ConsoleColor>
-    {
-        { ConsoleKey.D1, ConsoleColor.Red },
-        { ConsoleKey.D2, ConsoleColor.Green },
-        { ConsoleKey.D3, ConsoleColor.Blue },
-        { ConsoleKey.D4, ConsoleColor.Yellow },
-        { ConsoleKey.D5, ConsoleColor.Cyan },
-        { ConsoleKey.D6, ConsoleColor.Magenta },
-        { ConsoleKey.D7, ConsoleColor.White },
-        { ConsoleKey.D8, ConsoleColor.Gray }
-    };
+        {
+            { ConsoleKey.D1, ConsoleColor.Red },
+            { ConsoleKey.D2, ConsoleColor.Green },
+            { ConsoleKey.D3, ConsoleColor.Blue },
+            { ConsoleKey.D4, ConsoleColor.Yellow },
+            { ConsoleKey.D5, ConsoleColor.Cyan },
+            { ConsoleKey.D6, ConsoleColor.Magenta },
+            { ConsoleKey.D7, ConsoleColor.White },
+            { ConsoleKey.D8, ConsoleColor.Gray }
+        };
 
-        static void DrawInConsole()
+        static void DrawInConsole(bool resumePreviousDrawing)
         {
             Console.CursorVisible = false;
+
+            if (resumePreviousDrawing)
+            {
+                foreach (var item in trail)
+                {
+                    DrawCharacter(item.x, item.y, item.character, item.color);
+                }
+            }
 
             DrawCursor();
 
@@ -189,10 +328,10 @@ namespace MenuForDrawing
                         break;
                     case ConsoleKey.Spacebar:
                         trail.Add((x, y, currentChar, currentColor));
+                        hasPreviousDrawing = true;
                         DrawCharacter(x, y, currentChar, currentColor);
                         break;
                     case ConsoleKey.Escape:
-
                         return;
                 }
 
@@ -210,8 +349,6 @@ namespace MenuForDrawing
                 {
                     ClearCursorArea(prevX, prevY);
                     DrawCursor();
-
-
                 }
             }
         }
@@ -231,7 +368,7 @@ namespace MenuForDrawing
 
             Console.SetCursorPosition(0, Console.WindowHeight - 1);
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"Kurzor pozíció: ({x}, {y}) | Prev. Kurzor pozíció: ({prevX}, {prevY}) | Szín: {currentColor} | Karakter: {currentChar} ");
+            Console.Write($"Kurzor pozíció: ({x}, {y}) | Prev. Kurzor pozíció: ({prevX}, {prevY}) | Szín: {currentColor} | Karakter: {currentChar}");
         }
 
         static void ClearCursorArea(int posX, int posY)
